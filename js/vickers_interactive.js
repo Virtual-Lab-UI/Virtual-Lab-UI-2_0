03da80432d980742c312f89b6a77f040973957aa
GioByte10 <gvanni.bernal10@gmail.com>
2023-11-07 20:37:12 -0800

    Added start time of vickers video

--- js/vickers_interactive.js
+++ js/vickers_interactive.js
@@ -65,7 +65,7 @@ let parent = document.getElementById("video_canvas");
 let data = {};
 data["time"] = new Date()
 data["time"].setHours(data["time"].getHours() - 8);
-data["time"].setSeconds(data["time"].getSeconds() - vickersTimestamps["test_steel"]);
+data["time"].setSeconds(data["time"].getSeconds() + vickersTimestamps["test_steel"]);
 
 let k = 0;
 function preload(){
